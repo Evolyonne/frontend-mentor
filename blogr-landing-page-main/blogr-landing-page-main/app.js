8b77eb2bdf18969238d283f0f90df8eb74bdde62
Laury Rojas <laury012@hotmail.fr>
2021-05-28 15:38:01 +0200

    start burger

--- blogr-landing-page-main/blogr-landing-page-main/app.js
+++ blogr-landing-page-main/blogr-landing-page-main/app.js
@@ -1,1 +1,7 @@
 const burger = document.querySelector('.icon-burger');
+const nav = document.querySelector('.menu-list')
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  nav.classList.toggle('nav_open');
+});
