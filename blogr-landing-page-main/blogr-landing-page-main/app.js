443bc6fdef26a54818e598ba0785d04dae544c63
Laury Rojas <laury012@hotmail.fr>
2021-06-07 15:34:17 +0200

    menu drop-down

--- blogr-landing-page-main/blogr-landing-page-main/app.js
+++ blogr-landing-page-main/blogr-landing-page-main/app.js
@@ -1,7 +1,10 @@
 const burger = document.querySelector('.icon-burger');
-const nav = document.querySelector('.menu-list')
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('.menu-list');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
+  menu.classList.toggle('nav_open');
   nav.classList.toggle('nav_open');
 });
+
